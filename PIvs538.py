871d01c9caf4ff993db004405fd18e0a0619df2b
Jack <P1h3r1e3d13@gmail.com>
2016-09-08 20:53:25 -0700

    Wrote comments
    Some very minor code edits, too.

--- PIvs538.py
+++ PIvs538.py
@@ -1,23 +1,25 @@
-'''
-PIvs538.py
+#
+# PIvs538.py
+# 
+# Compares FiveThirtyEight's odds for their "states to watch" with PredictIt
+# prices for each.
+# PI prices are grabbed from their API, but 538 odds are manually entered below.
+# 538 doesn't seem to have an API, but it would be nice to find a way to scrape
+# their data.
+# 
+# Jack Enneking
+# 2016-09-08
+# 
 
-Compares FiveThirtyEight's odds for their "states to watch" with PredictIt
-prices for each.
-PI prices are grabbed from their API, but 538 odds are manually entered below.
-538 doesn't seem to have an API, but it would be nice to find a way to scrape
-their data.
-
-Jack Enneking
-2016-09-08
-
-'''
 
 import requests
-import pprint
 from time import sleep
 
 
 ############  FiveThirtyEight data  ############
+# It would be very nice to find a way to scrape this, but for now it has to be
+# entered manually.
+# Format: Democrat chance on the left, Republican on the right.
 
 fteStates = {
 	'AZ': (33.2, 66.8),
@@ -39,15 +41,16 @@ fteStates = {
 
 ############  Get PredictIt data  ############
 
-tries = 5
+tries = 5	# Times to retry each request if it fails.
 
 def getContentDict(url, tries=5, delay=1):
+	# gets data from the API and starts parsing it
 	for i in range(tries):
 		r = requests.get(url)
-		if r.status_code == 200 and r.content != b'null':
-			return(r.json()['Contracts'])
-		sleep(delay)
-	raise
+		if r.status_code == 200 and r.content != b'null':	# PI gives a 200 for nonexistent markets, just with null contents.
+			return(r.json()['Contracts'])	# Extracts the good bits: a list of the (two) contracts.
+		sleep(delay)	# wait before retrying
+	raise	# if all tries fail
 
 urlBase = "https://www.predictit.org/api/marketdata/ticker/"	# all urlBase belongs to us
 suffix = "USPREZ16"
@@ -66,12 +69,12 @@ piStates = {
 	'PA': {'dem': None,'gop': None},
 	'VA': {'dem': None,'gop': None},
 	'WI': {'dem': None,'gop': None},
-}
+}	# the main dict
 
 for state in sorted(piStates):
-	print("Get prices for " + state + " ... ", end="", flush=True)
+	print("Get prices for " + state + "... ", end="", flush=True)	# Let the user know we're trying
 	
-	url = urlBase + state + "." + suffix
+	url = urlBase + state + "." + suffix	# e.g. "https://www.predictit.org/api/marketdata/ticker/AZ.USPREZ16"
 	
 	try:
 		contracts = getContentDict(url, 5)
@@ -79,22 +82,23 @@ for state in sorted(piStates):
 		print("fail!")
 	else:
 		print("good!")
-		for contract in contracts:
+		for contract in contracts:	# contracts should be a list of the two contracts for the state
 			if contract['Name'] == "Democratic":
-				piStates[state]['dem'] = contract['BestBuyYesCost']
+				piStates[state]['dem'] = contract['BestBuyYesCost']	# put the Y cost in the main dict
 			elif contract['Name'] == "Republican":
-				piStates[state]['gop'] = contract['BestBuyYesCost']
+				piStates[state]['gop'] = contract['BestBuyYesCost']	# put the Y cost in the main dict
 			else:
-				print("Something fishy, though.")
+				print("Something fishy, though.")	# not Democratic or Republican
 
 print("")
 
 
 ############  Printing  ############
 
-colWidths = [4,3,4,4]
+colWidths = [4,3,4,4]	# adjust table spacing here
 
 def addSign(n):
+	# formats diffs for printing
 	if int(n) > 0:
 		s = "+" + format(n, '0.0f')
 	else:
@@ -121,14 +125,14 @@ header2 = " ".join((
 
 print(header1)
 print(header2)
-print('-' * len(header2))
+print('-' * len(header2))	# bar under headers
 
-for state in sorted(piStates):
-	fteDemNormal = fteStates[state][0] / 100
-	fteDemPercent = format(fteDemNormal * 100, '0.0f') + "%"
-	piDemNormal = piStates[state]['dem']
-	piDemPercent = format(piDemNormal * 100, '0.0f') + "\u00A2"
-	demDiff = addSign((piDemNormal - fteDemNormal) * 100)
+for state in sorted(piStates):	# sort to print alphabetically
+	fteDemNormal = fteStates[state][0] / 100	# 538's prediction normalized to 0-1 scale
+	fteDemPercent = format(fteDemNormal * 100, '0.0f') + "%"	# back up to percent scale and formatted for printing
+	piDemNormal = piStates[state]['dem']	# PI price from main dict
+	piDemPercent = format(piDemNormal * 100, '0.0f') + "\u00A2"	# formatted for printing with cent sign
+	demDiff = addSign((piDemNormal - fteDemNormal) * 100)	# maybe I should just work in percent scale to begin with
 	
 	fteGopNormal = fteStates[state][1] / 100
 	fteGopPercent = format(fteGopNormal * 100, '0.0f') + "%"
@@ -146,4 +150,5 @@ for state in sorted(piStates):
 		fteGopPercent.rjust(colWidths[1]),
 		piGopPercent.rjust( colWidths[2]),
 		gopDiff.rjust(      colWidths[3]),
-	)
+	)	# the goods!
+# Happy trading!
